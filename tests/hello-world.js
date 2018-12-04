913d2d8670087eceefbb78742f707fc22a090c38
Tomas Bravo <tomas@leeruniek.nl>
2018-12-04 10:32:26 +0100

    Add test to ensure HelloWorld changes color based on a 'color' prop of type Color

--- tests/hello-world.js
+++ tests/hello-world.js
@@ -2,13 +2,28 @@ import tape from "tape";
 import React from "react";
 import TestRenderer from "react-test-renderer";
 
+import type { Color } from "../src/types";
 import HelloWorld from "../src/hello-world.jsx";
 
-tape("Hello World", t => {
+tape("is a span containing the text 'Hello World!'", t => {
   t.plan(2);
 
-  let component = TestRenderer.create(<HelloWorld />).toJSON();
+  const component = TestRenderer.create(<HelloWorld />).toJSON();
 
   t.equal(component.type, "span");
   t.deepEqual(component.children, ["Hello World!"]);
+});
+
+tape("changes color based on the 'color' prop", t => {
+  t.plan(4);
+
+  const noColor = TestRenderer.create(<HelloWorld />).toJSON();
+  const red = TestRenderer.create(<HelloWorld color={"red"} />).toJSON();
+  const green = TestRenderer.create(<HelloWorld color={"green"} />).toJSON();
+  const blue = TestRenderer.create(<HelloWorld color={"blue"} />).toJSON();
+
+  t.ok(noColor.props.className.includes("red"));
+  t.ok(red.props.className.includes("red"));
+  t.ok(green.props.className.includes("green"));
+  t.ok(blue.props.className.includes("blue"));
 });
