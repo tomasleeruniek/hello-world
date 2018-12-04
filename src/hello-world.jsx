9015107366ee372613e714c8aa2fc543a6979c10
Tomas Bravo <tomas@leeruniek.nl>
2018-12-04 09:15:56 +0100

    Vary CSS class of HelloWorld based on an optional Color prop

--- src/hello-world.jsx
+++ src/hello-world.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import classnames from "classnames";
+import type { Color } from "./types";
 
 import "./hello-world.css";
 
-const HelloWorld = React.memo(() => (
-  <span styleName="hello-world">Hello World!</span>
+type Props = {
+  color?: Color
+};
+
+const HelloWorld = React.memo(({ color = "red" }) => (
+  <span styleName={classnames("hello-world", color)}>Hello World!</span>
 ));
 
 export default HelloWorld;
